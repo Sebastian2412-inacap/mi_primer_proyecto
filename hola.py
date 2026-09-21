77fef3c28b4200f3591701fe7a1815e6c7e98a8a
unknown <sistemas@VRE_LAB20919.inacap.cl>
2026-09-21 17:26:18 -0300

    mi primer commit para probar

--- hola.py
+++ hola.py
@@ -1,1 +1,19 @@
 print('buenas tardes gente, no se que estoy haciendo xd')
+
+valor=0
+
+while valor==0:
+
+    print('¿corneta?: si/no')
+    respuesta=input(str('Diga respuesta'.lower))
+
+    if respuesta == 'si':
+
+        print('¿gay?')
+        valor=1
+
+    else:
+
+        print('si te gusta')
+
+print('fin del programa')
